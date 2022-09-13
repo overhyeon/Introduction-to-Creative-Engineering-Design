99189033cba08db3ed19623b7a4ade609233e47d
overhyeon <105336318+overhyeon@users.noreply.github.com>
2022-09-13 14:30:14 +0900

    Update 04_example_3.ino

--- 04_example_3/04_example_3.ino
+++ 04_example_3/04_example_3.ino
@@ -17,10 +17,10 @@ void loop() {
   toggle = toggle_state(toggle);
   digitalWrite(PIN_LED, toggle);
   delay(1000);
-  digitalWrite(PIN_LED, 1);
-  delay(1000);
+//   digitalWrite(PIN_LED, 1);
+//   delay(1000);
 }
 
 int toggle_state(int toggle) {
-  return toggle;
+  return !toggle;
 }
